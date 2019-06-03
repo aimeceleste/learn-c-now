79fdb30588faa97408ba0f7b8605471cfbb67a6a
Celeste Thayer <aimeceleste@gmail.com>
2019-06-02 18:14:35 -0700

    Upload the finished game.
    Consider refactoring from a struct board to an array board, someday. Would be a lot simpler to update and test the board.

--- ch11-q9.c
+++ ch11-q9.c
@@ -29,99 +29,215 @@ struct board_st {
     int spot9;
 };
 /* Global board structure */
-struct board_st b = {
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9,
-};
+struct board_st b = {};
+
+/* Global variables */ 
+// flip a coin / random number gen a first mover OR select always human moves first
+// X = human O = computer (temp: human move 2)
+int numPlayers = 2;
+// first player is 88 for X
+int curPlayer = 1;
+int counter = 1; 
 
 /* Function primitives */ 
 int drawBoard(struct board_st b);       /* draws the board */ 
-int move(int spot, char val);           /* a move puts a value (X or O) into the designated spot */  
-bool canMove(void);                     /* determines whether there is any legal move. Returns false if no legal moves available */ 
+int move(int spot);                     /* a move puts a value (X or O) into the designated spot */  
+bool canMove(void);                     /* determines whether there is any legal move. Returns false if all board spots are full */ 
+bool checkWin(int curPlayer);           /* check if the current player has won or not */
+bool isValid(int spot);                 /* check to see if the spot is a valid spot to move to (i.e. is a 0) */
 
 int main(void) {
-    // flip a coin / random number gen a first mover OR select always human moves first
-    // X = human O = computer (temp: human move 2)
-    int numPlayers = 2;
-    int curPlayer = 0;
+// simple test to tell that my original, sans-break case statement wasn't working, 
+// also makes a good unit test for small changes: 
+// drawBoard(b);
+// move(1, 100);
+// drawBoard(b);
+
+    printf("Begin your game.\n");
+    printf("Hit 1 for the first spot, 2 for the second, and so on...\n");
 
     while (1) {     // begin the play loop
-        printf("Pick a spot:\n");
-        drawBoard(b);
-        // check for legal moves, if none it's a tie 
-        if (canMove() = false) {
-            printf("You have tied.");
-            break;
-        }
-        // ascii X = 88, O = 79
-            
-        // move a player 
-        // check if win - if yes, break
-        // switch player
-        // back to front of loop if not win
 
-    }
+        if (canMove() == false) {
+            drawBoard(b);
+            printf("You have tied.\n");
+            break; 
+        } 
+        drawBoard(b);
+        // ask to move a player
+        printf("Pick a spot.\n");
+
+        int spot;
+        scanf("%d", &spot);
+        if (isValid(spot) == false ) {
+            printf("Invalid move.\n");
+            continue;
+        }
+        move(spot);
+
+        // check if win - if yes, break
+        if (checkWin(curPlayer) == true ) {
+            drawBoard(b);
+            printf("Player %i has won - congrats!\n", curPlayer);
+            break;
+        } 
+
+        // swap players
+        curPlayer = curPlayer == 1 ? 2 : 1 ; 
+        // back to front of loop if not win 
+    } 
+
     return 0;
 }
 
 int drawBoard(struct board_st b) {
-    printf("%d | %d | %d\n", b.spot1, b.spot2, b.spot3);
+    char spot1 = b.spot1 == 0 ? '1' : b.spot1;
+    char spot2 = b.spot2 == 0 ? '2' : b.spot2;
+    char spot3 = b.spot3 == 0 ? '3' : b.spot3;
+    char spot4 = b.spot4 == 0 ? '4' : b.spot4;
+    char spot5 = b.spot5 == 0 ? '5' : b.spot5;
+    char spot6 = b.spot6 == 0 ? '6' : b.spot6;
+    char spot7 = b.spot7 == 0 ? '7' : b.spot7;
+    char spot8 = b.spot8 == 0 ? '8' : b.spot8;
+    char spot9 = b.spot9 == 0 ? '9' : b.spot9;
+
+    printf("%c | %c | %c\n", spot1, spot2, spot3);
     printf("----------\n");
-    printf("%d | %d | %d\n", b.spot4, b.spot5, b.spot6);
+    printf("%c | %c | %c\n", spot4, spot5, spot6);
     printf("----------\n");
-    printf("%d | %d | %d\n", b.spot7, b.spot8, b.spot9);
+    printf("%c | %c | %c\n", spot7, spot8, spot9);
 }
 
-int move(int spot, char val) {
+int move(int spot) {
+    char m = curPlayer == 1 ? 'x' : 'o';
+    
     switch (spot) {
+        // add a break duh...this is where I learned about https://en.wikipedia.org/wiki/Duff%27s_device
+        // and the fact that a break is a good practice in a switch-case statement
+        // because despite what I learned from Learn C Now, a break seems to be what my compiler needs
+        // or else it doesn't ever do just one case statement, it applys all of them. 
         case 1:
-            b.spot1 = val;
+            b.spot1 = m;
+            break;
         case 2:
-            b.spot2 = val;
+            b.spot2 = m;
+            break;
         case 3: 
-            b.spot3 = val;
+            b.spot3 = m;
+            break;
         case 4: 
-            b.spot4 = val;
+            b.spot4 = m;
+            break;
         case 5:
-            b.spot5 = val;
+            b.spot5 = m;
+            break;
         case 6: 
-            b.spot6 = val;
+            b.spot6 = m;
+            break;
         case 7: 
-            b.spot7 = val;
+            b.spot7 = m;
+            break;
         case 8: 
-            b.spot8 = val;
+            b.spot8 = m;
+            break;
         case 9: 
-            b.spot9 = val;
+            b.spot9 = m;
+            break;
     }
 }
 
-bool canMove(void) {
-    if (b.spot1 < 79) {
-        return true;
-    } else if (b.spot2 < 79) {
-        return true;
-    } else if (b.spot3 < 79) {
-        return true;
-    } else if (b.spot4 < 79) {
-        return true;
-    } else if (b.spot5 < 79) {
-        return true;
-    } else if (b.spot6 < 79) {
-        return true;
-    } else if (b.spot7 < 79) {
-        return true;
-    } else if (b.spot8 < 79) {
-        return true;
-    } else if (b.spot9 < 79) {
-        return true;
+bool canMove(void) { 
+    if (b.spot1 == 0 || b.spot2 == 0 || b.spot3 == 0 || b.spot4 == 0 || 
+        b.spot5 == 0 || b.spot6 == 0 || b.spot7 == 0 || b.spot8 == 0 ||
+        b.spot9 == 0) {
+        return true; 
     } else {
         return false;
     }
+}
+
+bool checkWin(int curPlayer) {
+    if (b.spot1 != 0 && b.spot1 == b.spot2 && b.spot2 == b.spot3) {
+        //printf("testme");
+        return true; 
+    } else if (b.spot4 != 0 && b.spot4 == b.spot5 && b.spot5 == b.spot6) {
+        return true; 
+    } else if (b.spot7 != 0 && b.spot7 == b.spot8 && b.spot8 == b.spot9) {
+        return true; 
+    } else if (b.spot1 != 0 && b.spot1 == b.spot5 && b.spot5 == b.spot9) {
+        return true; 
+    } else if (b.spot7 != 0 && b.spot7 == b.spot5 && b.spot5 == b.spot3) {
+        return true; 
+    } else if (b.spot1 != 0 && b.spot1 == b.spot4 && b.spot4 == b.spot7) {
+        return true; 
+    } else if (b.spot2 != 0 && b.spot2 == b.spot5 && b.spot5 == b.spot8) {
+        return true; 
+    } else if (b.spot3 != 0 && b.spot3 == b.spot6 && b.spot6 == b.spot9) {
+        return true; 
+    }
+    else {
+        // printf("Condition null");
+        return false;
+    }
+}
+
+bool isValid(int spot) {
+    int m = 0; 
+    switch (spot) {
+        case 1:
+            m = b.spot1;
+            if ( m == 0 ) {
+                return true;
+            }
+            break;
+        case 2:
+            m = b.spot2;
+            if ( m == 0 ) {
+                return true;
+            }
+            break;
+        case 3: 
+            m = b.spot3;
+            if ( m == 0 ) {
+                return true;
+            }
+            break;
+        case 4: 
+            m = b.spot4;
+            if ( m == 0 ) {
+                return true;
+            }            
+            break;
+        case 5:
+            m = b.spot5;
+            if ( m == 0 ) {
+                return true;
+            }            
+            break;
+        case 6: 
+            m = b.spot6;
+            if ( m == 0 ) {
+                return true;
+            }            
+            break;
+        case 7: 
+            m = b.spot7;
+            if ( m == 0 ) {
+                return true;
+            }
+            break;
+        case 8: 
+            m = b.spot8; 
+            if ( m == 0 ) {
+                return true;
+            }            
+            break;
+        case 9: 
+            m = b.spot9;
+            if ( m == 0 ) {
+                return true;
+            }            
+            break;
+    }
+    return false;
 }
